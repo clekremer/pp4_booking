3b6932eaff15d45ce152157077f1b4590c804dbb
clemens kremer <molkrich@gmx.de>
2024-06-25 21:32:48 +0000

    basic structure

--- bookingsystem/urls.py
+++ bookingsystem/urls.py
@@ -15,13 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import booking_view
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('appointment/', include('appointment.urls')),
-    path('book/', booking_view, name='booking'),
     path('booking/', include('booking.urls')),
 ]
-
